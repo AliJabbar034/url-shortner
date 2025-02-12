f3a2b7c73bd521d68a2a322c65603635b949fb0c
alijabbar034 <alijabbar0034@gmail.com>
2025-02-12 23:06:40 +0500

    added redis db

--- routes/resolve.go
+++ routes/resolve.go
@@ -1,1 +1,33 @@
 package routes
+
+import (
+	"github.com/alijabbar034/url-shortner/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+
+	url := c.Params("url")
+	r := database.CreateClient(0)
+
+	defer r.Close()
+	value, err := r.Get(database.CTX, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"code": fiber.StatusNotFound,
+			"msg":  "url not found",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code": fiber.StatusInternalServerError,
+			"msg":  err.Error(),
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+	_ = rInr.Incr(database.CTX, "counter")
+
+	return c.Redirect(value, 301)
+}
